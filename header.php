588aee3ef180f9ba9d8c06fd7d013c50633cf34d
David Lewis <mail@david-lewis.com>
2019-11-10 16:56:07 +1100

    update fonts

--- header.php
+++ header.php
@@ -2,11 +2,16 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="utf-8" />
- 	<title><?php wp_title( '|', true, 'right' ); ?> David Lewis</title>
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+
+	 <link
+      href="https://fonts.googleapis.com/css?family=Raleway|Roboto+Slab&display=swap"
+      rel="stylesheet"
+    />
+	 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php wp_title( '|', true, 'right' ); ?> David Lewis</title>
 	<?php wp_head(); ?>
 </head>
-
+<body class="antialiased">
 <div>David Lewis</div>
 <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
