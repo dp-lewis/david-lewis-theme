9843ee2e498f362a584ccd7b3cb2325a76777066
David Lewis <mail@david-lewis.com>
2019-11-10 17:03:32 +1100

    update post template

--- single-post.php
+++ single-post.php
@@ -5,38 +5,11 @@ get_header();
       <header class="mb-8">
             <div class="mb-5 inline-block text-sm font-medium bg-blue-500 py-1 px-2 rounded text-blue-100 align-middle">Community</div>
         <h1 class="text-5xl leading-none mb-2">
-          Dealing with a meetup organisers worst nightmare
+          <?php the_title(); ?>
         </h1>
-        <time class="block mb-2 text-sm text-gray-600" datetime="2019-01-01">1st January 2019</time>
+        <time class="block mb-2 text-sm text-gray-600" datetime="<?php the_time('F jS, Y') ?>"><?php the_time('F jS, Y') ?></time>
       </header>
-      <p>
-        On Thursday 4th February we held our 17th SydCSS. It was going to be an
-        easy event for us. Speakers were lined up, the venue was booked, we had
-        people at the venue prepped ready to set up the space as we needed it.
-        We even had a little extra sparkle lined up in the form of the CSS
-        Working Group being in town, and one of their co-chairs lined up as a
-        surprise speaker. All was going swimmingly, but then at 11:48am on the
-        day of the event we received a call that there was a major problem at
-        the venue and they might not be able to host us.
-      </p>
-
-      <img src="https://www.david-lewis.com/wp-content/uploads/2016/02/ciab-768x512.jpg" />
-
-      <p>
-        If you run a meetup, this is a disaster. SydCSS can easily attract over
-        100 people and finding a new location with only 6 hours and 42 minutes
-        before you open the doors is not an easy task. But if you go and look at
-        the feedback we’ve had from that night you’ll see that it was one of our
-        most popular events.
-      </p>
-
-      <p>
-        How did we pull together the event at the last minute, and avoid having
-        to cancel? Fiona has written a wonderful post about how the SydCSS
-        community rallied around us to help pull this off. This post is a
-        companion Fiona’s, to help other event organisers keep a cool head and
-        work to find a solution to problems. Here’s how we played it:
-      </p>
+      <?php the_content(); ?>
     </article>
-    <?php
-    get_footer();
+<?php
+get_footer();
