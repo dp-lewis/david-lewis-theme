9e5a780b9b569cc4a708263b8349af2c386572ef
David Lewis <mail@david-lewis.com>
2019-11-10 16:27:46 +1100

    Add tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {},
+      fontFamily: {
+        'display': ['Roboto Slab', 'serif'],
+        'body': ['Raleway', 'sans-serif'],
+      }
+    },
+    variants: {},
+    plugins: []
+  }
+  
