01212571892b05e65860128e3bdee46f3bc4a909
David Lewis <mail@david-lewis.com>
2019-11-11 21:01:19 +1100

    update html with new elements

--- header.php
+++ header.php
@@ -13,13 +13,12 @@
 	<?php wp_head(); ?>
 </head>
 <body class="antialiased pb-24 bg-gray-200 max-w-3xl m-auto">
-<nav
-      class="fixed z-10 w-full bottom-0 left-0 flex items-center justify-center text-white"
-    >
-      <a href="/" class="p-4">Home</a>
-      <a href="/#dev" class="p-4">Dev & Design</a>
-      <a href="/#community" class="p-4">Community</a>
-    </nav>
+<nav class="fixed z-10 w-full bottom-0 left-0 flex items-center justify-center text-white">
+  <a href="/" class="p-4 md:p-8 hover:bg-blue-600">Home</a>
+  <a href="/#dev" class="p-4 md:p-8 hover:bg-blue-600">Dev & Design</a>
+  <a href="/#community" class="p-4 md:p-8 hover:bg-blue-600">Community</a>
+</nav>
+
 <?php 
 // wp_nav_menu( array( 'theme_location' => 'header-menu' ) );
  ?>
--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 get_header();
 ?>
-<div class="font-display p-4 uppercase text-center text-xl">David Lewis</div>
+<div class="p-4">
+    <div class="font-display uppercase text-center text-2xl">David Lewis</div>
+    <div class="font-body text-center text-sm">Keep tech fun</div>
+</div>
 <?php
 $latestPost = get_posts( array(
     'posts_per_page' => 1
@@ -29,7 +32,10 @@ $communityPosts = get_posts( array(
 if ( $communityPosts ) {
 ?>
 <section class="pl-4 pr-4 mb-4" id="community">
-    <header><h1 class="text-xl mb-2">Community</h1></header>
+    <header>
+      <h1 class="text-xl mb-0">Community</h1>
+      <h2 class="text-xs mb-4">I&apos;m a regular host at SydCSS with an ongoing battle with public speaking anxiety.</h2>
+    </header>
     <div class="overflow-x-auto gradient">
         <div class="flex flex-no-wrap">
         <?php
@@ -53,7 +59,10 @@ $devdesignPosts = get_posts( array(
 if ( $devdesignPosts ) {
     ?>
 <section class="pl-4 pr-4 mb-4" id="dev">
-    <header><h1 class="text-xl mb-2">Design &amp; Development</h1></header>
+    <header>
+      <h1 class="text-xl mb-0">Design &amp; Development</h1>
+      <h2 class="text-xs mb-4">Whilst my day job doesn&apos;t require much hands on coding, I still like to get my hands dirty with tech.</h2>
+    </header>
     <div class="overflow-x-auto gradient">
         <div class="flex flex-no-wrap">
         <?php
