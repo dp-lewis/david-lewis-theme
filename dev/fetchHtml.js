6cd2b825c63d494c60f79b1b5c0a25f84d999040
David Lewis <mail@david-lewis.com>
2019-11-12 19:57:34 +1100

    update medium view

--- dev/fetchHtml.js
+++ dev/fetchHtml.js
@@ -1,5 +1,6 @@
 const request = require("request-promise");
 const fs = require("fs");
+const cheerio = require("cheerio");
 
 async function getHtml(url) {
     const html = await request.get(url);
@@ -12,11 +13,17 @@ function saveHtmlToFile(fileName, html) {
 }
 
 async function main() {
+    let $;
     const html = await getHtml('https://www.david-lewis.com');
-    saveHtmlToFile("./dev/index.html", html);
+
+    $ = cheerio.load(html);
+    $('link:nth-child(3)').attr('href', './style.css');
+    saveHtmlToFile("./dev/index.html", $.html());
 
     const article = await getHtml('https://www.david-lewis.com/when-you-have-no-work-from-home-policy-start-here/');
-    saveHtmlToFile("./dev/article.html", article);
+    $ = cheerio.load(article);
+    $('link:nth-child(3)').attr('href', './style.css');
+    saveHtmlToFile("./dev/article.html", $.html());
 }
 
 main();
